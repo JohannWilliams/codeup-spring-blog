9bb114b777f52aed5323560bc759121ec8570823
Johann Williams <johann.williams01@gmail.com>
2023-08-09 10:50:17 -0500

    updated post get and post mapping to include the posibility of training /

--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -15,24 +15,24 @@ public class PostController {
         this.postDao = postDao;
     }
 
-    @GetMapping("/posts")
+    @GetMapping({"/posts", "/posts/"})
     public String getPosts(Model model){
         model.addAttribute("posts", postDao.findAll());
         return "posts/index";
     }
 
-    @GetMapping("/posts/{id}")
+    @GetMapping({"/posts/{id}", "/posts/{id}/"})
     public String getPosts(@PathVariable String id, Model model){
         model.addAttribute("post", postDao.getById(Long.parseLong(id)));
         return "posts/show";
     }
 
-    @GetMapping("/posts/create")
+    @GetMapping({"/posts/create", "/posts/create/"})
     public String createPostView(){
         return "posts/create";
     }
 
-    @PostMapping("/posts/create")
+    @PostMapping({"/posts/create", "/posts/create/"})
     public String createPostPost(@RequestParam(name = "title") String title, @RequestParam(name = "body") String body){
         postDao.save(new Post(title, body));
         return "redirect:/posts";
